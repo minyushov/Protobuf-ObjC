34a9ea5a56c32c56f29019161cbf150590ff389b
Robert Carlsen <robert@projectfla.com>
2014-02-11 15:47:11 -0500

    correcting return type in convert method

--- src/runtime/Classes/Utilities.h
+++ src/runtime/Classes/Utilities.h
@@ -25,7 +25,7 @@ Float32 convertInt32ToFloat32(int32_t f);
 uint64_t convertInt64ToUInt64(int64_t i);
 int64_t  convertUInt64ToInt64(uint64_t u);
 uint32_t convertInt32ToUInt32(int32_t i);
-int64_t  convertUInt32ToInt32(uint32_t u);
+int32_t  convertUInt32ToInt32(uint32_t u);
 
 int32_t logicalRightShift32(int32_t value, int32_t spaces);
 int64_t logicalRightShift64(int64_t value, int32_t spaces);
--- src/runtime/Classes/Utilities.m
+++ src/runtime/Classes/Utilities.m
@@ -72,7 +72,7 @@ uint32_t convertInt32ToUInt32(int32_t v) {
 }
 
 
-int64_t convertUInt32ToInt32(uint32_t v) {
+int32_t convertUInt32ToInt32(uint32_t v) {
   union { int32_t i; uint32_t u; } u;
   u.u = v;
   return u.i;
